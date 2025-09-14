b3510dbca5e4b6b1434f3fbe234fe6b680149160
Bohdan11Dii <bodyabrovdiy@gmail.com>
2025-09-14 16:02:06 +0200

    Update base templates and sidebar, adjust URLs

--- admin_panel/urls.py
+++ admin_panel/urls.py
@@ -1,12 +1,21 @@
 from django.urls import path
-from admin_panel.views import hello_world, SeoBlockListView, SeoBlockCreateView, SeoBlockUpdateView, SeoBlockDeleteView
+
+from admin_panel.views import hello_world, SeoBlockListView, SeoBlockCreateView, SeoBlockUpdateView, SeoBlockDeleteView, \
+    MainPageListView, MainPageCreateView, MainPageUpdateView, MainPageDeleteView
 
 urlpatterns = [
     path("", hello_world, name="hello_world"),
+
     path("seo_block/", SeoBlockListView.as_view(), name="seo_block_list"),
     path("seo_block/create/", SeoBlockCreateView.as_view(), name="seo_block_create"),
     path("seo_block/update/<int:pk>", SeoBlockUpdateView.as_view(), name="seo_block_update"),
     path("seo_block/delete/<int:pk>", SeoBlockDeleteView.as_view(), name="seo_block_delete"),
+
+    path("main/", MainPageListView.as_view(), name="main_page_list"),
+    path("main/create/", MainPageCreateView.as_view(), name="main_page_create"),
+    path("main/update/<int:pk>", MainPageUpdateView.as_view(), name="main_page_update"),
+    path("main/delete/<int:pk>", MainPageDeleteView.as_view(), name="main_page_delete"),
+
 ]
 
 app_name = "admin_panel"
