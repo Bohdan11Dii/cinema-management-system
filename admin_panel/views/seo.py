bac16059e279195728ac6e519729943c86a25578
Bohdan11Dii <bodyabrovdiy@gmail.com>
2025-09-14 12:33:13 +0200

    Realized the full CRUD for Seo

--- admin_panel/views/seo.py
+++ admin_panel/views/seo.py
@@ -16,3 +16,16 @@ class SeoBlockCreateView(generic.CreateView):
     template_name = "seo/seo_form.html"
     form_class = SeoForm
     success_url = reverse_lazy("admin_panel:seo_block_list")
+
+
+class SeoBlockUpdateView(generic.UpdateView):
+    model = SeoBlock
+    form_class = SeoForm
+    template_name = "seo/seo_form.html"
+    success_url = reverse_lazy("admin_panel:seo_block_list")
+
+class SeoBlockDeleteView(generic.DeleteView):
+    model = SeoBlock
+    template_name = "seo/seo_confirm_delete.html"
+    success_url = reverse_lazy("admin_panel:seo_block_list")
+
